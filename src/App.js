4d73f9ace18cfa4961486bbc027d60967862e3b0
johnpark3002 <john.park2@mail.mcgill.ca>
2024-10-28 19:06:40 -0400

    Added some image components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/navigation-bar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section id="navigation">
+        <NavBar />
+      </section>
+      <section id="home">Home Content</section>
+      <section id="about">About Content</section>
+      <section id="projects">Projects Content</section>
+      <section id="skills">Skills Content</section>
+      <section id="experience">Experience Content</section>
+      <section id="contanct">Contact Content</section>
     </div>
   );
 }
--- src/components/navigation-bar.jsx
+++ src/components/navigation-bar.jsx
@@ -1,7 +1,19 @@
+import logo from "../images/logo.jpeg";
+
 export default function NavBar() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <nav className="navigation-bar">
+      <div className="navigation-bar-logo">
+        <img src={logo} alt="Logo" width={100} height={100}></img>
+      </div>
+      <div className="navigation-bar-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#experience">Experience</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
 }
