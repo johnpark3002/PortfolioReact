28e785521de19d1a1b20d263ed134c42602bf384
John Park <john.park2@mail.mcgill.ca>
2024-11-25 22:45:33 -0500

    Started working on projects section

---
+++ src/components/project-card.jsx
@@ -0,0 +1,20 @@
+export default function ProjectCard() {
+  return (
+    <>
+      <div className="card">
+        <div className="card-front">
+          <h1>Project Title 1</h1>
+          <h3>Description</h3>
+        </div>
+        <div className="card-back">
+          <h2>Project Description</h2>
+          <h3>Project Purpose</h3>
+          <h3>Role and Contributions</h3>
+          <h3>Technical challenges</h3>
+          <h3>Key Achievements</h3>
+          <h3>Technologies Used</h3>
+        </div>
+      </div>
+    </>
+  );
+}
