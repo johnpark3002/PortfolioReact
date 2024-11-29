bbb5872258c3b992db38c74cc48d4429abbeb28e
John Park <john.park2@mail.mcgill.ca>
2024-11-29 00:26:07 -0500

    Added github links to relevant projects

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,4 +1,5 @@
 import "../styles/project-card.css";
+import github from "../images/github-logo.svg";
 
 export default function ProjectCard(props) {
   const projects = props.projects;
@@ -16,8 +17,6 @@ export default function ProjectCard(props) {
             </div>
           </div>
           <div className="card-back">
-            <div className="card-back-name">
-            </div>
             <div className="card-back-description">
               <span>Description: </span>
               {project.description}
@@ -26,6 +25,16 @@ export default function ProjectCard(props) {
               <span>Technologies Used: </span>
               {project.technologies}
             </div>
+            {project.url && (
+              <div className="card-back-github-logo">
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={github}
+                    alt="Github logo with links to the corresponding project."
+                  ></img>
+                </a>
+              </div>
+            )}
           </div>
         </div>
       ))}
